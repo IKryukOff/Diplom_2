d836f1ae5217e0147ec1fd49f69ce3cd89878c29
IKryukOff <ivan.kryukoff96@yandex.ru>
2025-05-31 07:38:06 +0100

    use pydantic model for create_user fixture

--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
 from typing import Generator
+
 import pytest
 from faker import Faker
 from methods.user_methods import UserMethods
+from models.user import CreatedUser
 
 
 @pytest.fixture()
@@ -13,8 +15,9 @@ def user_data() -> dict[str, str]:
 
 
 @pytest.fixture()
-def create_user(user_data: dict[str, str]) -> Generator[tuple[dict[str, str], str], None, None]:
+def create_user(user_data: dict[str, str]) -> Generator[CreatedUser, None, None]:
     _, response_data = UserMethods.register(user_data=user_data)
-    access_token = response_data['accessToken']
-    yield (user_data, access_token)
-    UserMethods.delete(access_token=access_token)
+    created_user = CreatedUser(register_data=user_data,
+                               response_data=response_data)
+    yield created_user
+    UserMethods.delete(access_token=created_user.access_token)
--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,6 +1,7 @@
 import allure
 import pytest
 from methods.user_methods import UserMethods
+from models.user import CreatedUser
 
 
 @allure.parent_suite('Тестирование API сервиса Stellar Burgers')
@@ -18,9 +19,9 @@ class TestCreateUser:
     @allure.title('Проверка того, что запрос возвращает сообщение "User already exists"')
     @allure.description('Выполняем POST запрос /api/auth/register с указанием data параметров '
                         '(email, пароль, имя) уже зарегестрированного ранее пользователя')
-    def test_create_already_exists_user_status_code_is_forbidden(
-            self, create_user: tuple[dict[str, str], str]) -> None:
-        status_code, response_data = UserMethods.register(user_data=create_user[0])
+    def test_create_already_exists_user_status_code_is_forbidden(self,
+                                                                 create_user: CreatedUser) -> None:
+        status_code, response_data = UserMethods.register(user_data=create_user.register_data)
         assert (status_code == 403 and
                 response_data['success'] is False and
                 response_data['message'] == 'User already exists')
@@ -29,10 +30,11 @@ class TestCreateUser:
     @allure.title('Проверка того, что запрос возвращает сообщение о необходимости указать все поля')
     @allure.description('Выполняем POST запрос /api/auth/register без указания email, пароля или '
                         'имени и ожидаем ошибку отсутствия необходимых данных')
-    @pytest.mark.parametrize('excluded_data_field', ['email', 'password', 'name'])
-    def test_create_user_with_incorrect_data_status_code_is_forbidden(
-            self, user_data: dict[str, str], excluded_data_field: str) -> None:
-        user_data.pop(excluded_data_field)
+    @pytest.mark.parametrize('excluded_field', ['email', 'password', 'name'])
+    def test_create_user_with_incorrect_data_status_code_is_forbidden(self,
+                                                                      user_data: dict[str, str],
+                                                                      excluded_field: str) -> None:
+        user_data.pop(excluded_field)
         status_code, response_data = UserMethods.register(user_data=user_data)
         assert (status_code == 403 and
                 response_data['success'] is False and
